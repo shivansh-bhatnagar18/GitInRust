3fe3bc5b916cacab36d12bda9d371b8a304fcc29
Shivansh Bhatnagar <shivansh.bhatnagar.mat22@itbhu.ac.in>
2024-06-12 00:36:01 +0530

    Read a blob object

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
-#[allow(unused_imports)]
+use flate2::read::ZlibDecoder;
 use std::env;
-#[allow(unused_imports)]
 use std::fs;
-
+use std::io::prelude::*;
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
+    // println!("Logs from your program will appear here!");
+    // println!("Logs from your program will appear here!");
+    // Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
     if args[1] == "init" {
         fs::create_dir(".git").unwrap();
         fs::create_dir(".git/objects").unwrap();
         fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+        fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
         println!("Initialized git directory")
+    } else if args[1] == "cat-file" {
+        let content =
+            fs::read(format!(".git/objects/{}/{}", &args[3][..2], &args[3][2..])).unwrap();
+        let mut z = ZlibDecoder::new(&content[..]);
+        let mut s = String::new();
+        z.read_to_string(&mut s).unwrap();
+        print!("{}", &s[8..]);
     } else {
         println!("unknown command: {}", args[1])
     }
